53c83e1511412f3e922733324f2625862043ddab
jsanz <jsanzv7@gmail.com>
2022-06-19 23:06:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.dobcalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 0123456789ABCDEF
+        // FFFFFFFF
+    }
+}
